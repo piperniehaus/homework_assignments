bf2e12cf3dc8565c42f58738c4643761204914f9
Piper Niehaus <piper.niehaus@gmail.com>
2015-02-23 17:54:19 -0700

    Refactor and simplify Fizz Buzz
    [#88678948]

--- methods_and_flow_control/fizz_buzz.rb
+++ methods_and_flow_control/fizz_buzz.rb
@@ -1,20 +1,14 @@
 #!/usr/bin/env ruby
 
-# # This program should output the numbers 1 through 100, however:
-# If evenly divisible by 3, print Fizz
-# If evenly divisible by 5, print Buzz
-# If evenly divisible by 3 and 5, print FizzBuzz
-# Otherwise, print out the number
+(1..100).each do |number|
+  result = number
+  if number % 15 == 0
+    result = 'FizzBuzz'
+  elsif number % 3 == 0
+    result = 'Fizz'
+  elsif number % 5 == 0
+    result = 'Buzz'
+  end
 
-(1..100).each do |n|
-  r_3 = n % 3
-  output = 'Fizz' * (1 - (r_3 / (r_3 + 1.0))).floor
-
-  r_5 = n % 5
-  output += 'Buzz' * (1 - (r_5 / (r_5 + 1.0))).floor
-
-  r = (n % 3) * (n % 5)
-  output += n.to_s * (r / (r + 1.0)).ceil
-
-  puts output.strip
+  puts result
 end
