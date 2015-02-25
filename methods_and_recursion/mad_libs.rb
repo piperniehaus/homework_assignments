8a2d9189924872e331d2f99d4a3f5ef93656d754
Piper Niehaus <piper.niehaus@gmail.com>
2015-02-24 18:48:21 -0700

    Refactor and simplify madlib
    88837752

--- methods_and_recursion/mad_libs.rb
+++ methods_and_recursion/mad_libs.rb
@@ -1,4 +1,4 @@
-#!usr/bin/env ruby
+#!/usr/bin/env ruby
 
 class Line
     attr_accessor :word
@@ -8,7 +8,7 @@ class Line
       @text = text
     end
     def get_word previous_word_type=''
-      if previous_word_type == ''
+      if word_type == ''
         @word = ''
       elsif @word_type == previous_word_type
         puts "Give me another #{word_type}:"
@@ -27,25 +27,25 @@ class Line
 end
 
 $lines = [
- Line.new("A vacation is when you take a trip to some\n", 'adjective'),
- Line.new('with your ','adjective'),
- Line.new(" family. Usually you go to some place that is near a/an\n",'noun'),
- Line.new(' or up on a/an ','noun'),
- Line.new("A good vacation place is one where you can ride\n",'plural noun'),
- Line.new('or play ','game'),
- Line.new(' or go hunting for ','plural noun'),
- Line.new('. I like to spend my time ','verb ending in ing'),
- Line.new(' or ','verb ending in ing'),
- Line.new(". When parents go on a vacation, \nthey spend their time eating three ",'plural noun'),
- Line.new(' a day, and fathers play golf, and mothers sit around ','verb ending in ing'),
- Line.new('. Last summer, my little brother fell in a/an _','noun'),
- Line.new(' and got poison ', 'plant'),
- Line.new(' all over his ', 'part of body'),
- Line.new('. My family is going to go to (the)','a place'),
- Line.new(", and I will practice \n",'verb ending in ing'),
- Line.new('. Parents need vacations more than kids because parents are always very ','adjective'),
- Line.new(' and because they have to work ','number'),
- Line.new(' hours every day all year making enough ', 'noun'),
+Line.new('A vacation is when you take a trip to some ', 'adjective'),
+Line.new(" place\n with your ",''),
+# Line.new(" family. Usually you go to some place that is near a/an\n",'noun'),
+# Line.new(' or up on a/an ','noun'),
+# Line.new("A good vacation place is one where you can ride\n",'plural noun'),
+# Line.new('or play ','game'),
+# Line.new(' or go hunting for ','plural noun'),
+# Line.new('. I like to spend my time ','verb ending in ing'),
+# Line.new(' or ','verb ending in ing'),
+# Line.new(". When parents go on a vacation, \nthey spend their time eating three ",'plural noun'),
+# Line.new(' a day, and fathers play golf, and mothers sit around ','verb ending in ing'),
+# Line.new('. Last summer, my little brother fell in a/an _','noun'),
+# Line.new(' and got poison ', 'plant'),
+# Line.new(' all over his ', 'part of body'),
+# Line.new('. My family is going to go to (the)','a place'),
+# Line.new(", and I will practice \n",'verb ending in ing'),
+# Line.new('. Parents need vacations more than kids because parents are always very ','adjective'),
+# Line.new(' and because they have to work ','number'),
+# Line.new(' hours every day all year making enough ', 'noun'),
 ]
 
 def get_words line, previous_line_type=''
@@ -55,7 +55,7 @@ def get_words line, previous_line_type=''
     get_words next_line, line.word_type
   else
     $lines.each do |line|
-      puts line.write
+      print line.write
     end
   end
 end
@@ -63,3 +63,6 @@ end
 get_words $lines[0]
 
 
+# $lines.each do |line|
+#   puts line.word_type
+# end
