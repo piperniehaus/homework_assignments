41ce3a0d885f93d7cafafe44cfb602e2cd6c3aec
Piper Niehaus <piper.niehaus@gmail.com>
2015-02-25 17:55:20 -0700

    Add string methods to string practice
    [#88678692]

--- methods_and_flow_control/string_practice.rb
+++ methods_and_flow_control/string_practice.rb
@@ -8,4 +8,4 @@ puts practice_string.upcase
 puts practice_string.capitalize.sub('string', "'string'")
 puts "The string '#{practice_string}' has #{practice_string.length} characters"
 puts practice_string.reverse
-puts 'practice practice practice!'
+puts "#{(practice_string.slice(/p.* /) * 3).strip}!"
