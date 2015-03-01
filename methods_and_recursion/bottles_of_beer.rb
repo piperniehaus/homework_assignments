26abb55f790c0f206a61e853ee3598ff5c151cff
Piper Niehaus <piper.niehaus@gmail.com>
2015-03-01 13:53:37 -0700

    Refactored 99 bottles of beer *shorten and simplify
    [#88573940]

--- methods_and_recursion/bottles_of_beer.rb
+++ methods_and_recursion/bottles_of_beer.rb
@@ -1,32 +1,26 @@
 #!/usr/bin/env ruby
-class Verse
-  attr_reader :number
-  def initialize(number_of_bottles)
-    @number = number_of_bottles
-  end
-  
-  def bottle_string
-    if @number > 1
-      "#{@number} bottles"
-    elsif @number == 1
-      '1 bottle'
-    else
-      'no more bottles'
-    end
-  end
 
-  def sing
-    next_bottles = Verse.new(@number - 1)
-    if @number > 0
-      puts "#{bottle_string} of beer on the wall, #{bottle_string} of beer."
-      print "Take one down and pass it around, "
-      puts "#{next_bottles.bottle_string} of beer on the wall.\n\n"
-      next_bottles.sing
-    else
-      puts 'No more bottles of beer on the wall, no more bottles of beer.'
-      puts 'Go to the store and buy some more, 99 bottles of beer on the wall.'
-    end
+def bottle_string(number)
+  if number > 1
+    "#{number} bottles"
+  elsif number == 1
+    '1 bottle'
+  else
+    'no more bottles'
   end
 end
 
-Verse.new(99).sing
+def sing(number)
+  if number > 0
+    puts "#{bottle_string(number)} of beer on the wall, "\
+    "#{bottle_string(number)} of beer."
+    puts 'Take one down and pass it around, '\
+    "#{bottle_string(number - 1)} of beer on the wall.\n\n"
+    sing(number - 1)
+  else
+    puts 'No more bottles of beer on the wall, no more bottles of beer.'
+    puts 'Go to the store and buy some more, 99 bottles of beer on the wall.'
+  end
+end
+
+sing(99)
